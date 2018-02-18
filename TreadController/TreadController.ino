3aebdce9f14a124014f90caafa764fc4f11da01d
Dylan Katz <3255634+muddmaker@users.noreply.github.com>
2018-02-17 16:16:08 -0800

    Add support for 4 wheels

--- TreadController/TreadController.ino
+++ TreadController/TreadController.ino
@@ -1,44 +1,63 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_DCMotor *left_mtr = AFMS.getMotor(1);
-Adafruit_DCMotor *right_mtr = AFMS.getMotor(2);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *left_front_mtr = AFMS.getMotor(2);
+Adafruit_DCMotor *right_front_mtr = AFMS.getMotor(1);
+Adafruit_DCMotor *left_back_mtr = AFMS.getMotor(3);
+Adafruit_DCMotor *right_back_mtr = AFMS.getMotor(4);
 int spd = 0;
+
+#define set_left_speed(s) do { \
+  left_front_mtr->setSpeed(s); \
+  left_back_mtr->setSpeed(s);  \
+} while (0)
+
+#define set_right_speed(s) do { \
+  right_front_mtr->setSpeed(s); \
+  right_back_mtr->setSpeed(s);  \
+} while (0)
+
+#define set_left_run(r) do { \
+  left_front_mtr->run(r);    \
+  left_back_mtr->run(r);     \
+} while (0)
+
+#define set_right_run(r) do { \
+  right_front_mtr->run(r);    \
+  right_back_mtr->run(r);     \
+} while (0)
 
 void setup() {
   Serial.begin(9600);
   AFMS.begin();
 
-  left_mtr->setSpeed(150);
-  left_mtr->run(FORWARD);
-  left_mtr->run(RELEASE);
-
-  right_mtr->setSpeed(150);
-  right_mtr->run(FORWARD);
-  right_mtr->run(RELEASE);
+  set_left_run(RELEASE);
+  set_right_run(RELEASE);
 }
 
 void loop() {
-  left_mtr->run(FORWARD);
-  right_mtr->run(FORWARD);
+  set_left_run(FORWARD);
+  set_right_run(FORWARD);
 
 #define SET_SPEEDS(x) do { \
-  left_mtr->setSpeed(x);   \
-  right_mtr->setSpeed(x);  \
+  set_left_speed(x);       \
+  set_right_speed(x);      \
   delay(10);               \
 } while (0)
 
   for (spd = 0; spd < 255; ++spd) SET_SPEEDS(spd);
   for (spd = 255; spd != 0; --spd) SET_SPEEDS(spd);
 
-  left_mtr->run(BACKWARD);
-  right_mtr->run(BACKWARD);
+  set_left_run(BACKWARD);
+  set_right_run(BACKWARD);
 
   for (spd = 0; spd < 255; ++spd) SET_SPEEDS(spd);
   for (spd = 255; spd != 0; --spd) SET_SPEEDS(spd);
 
-  left_mtr->run(RELEASE);
-  right_mtr->run(RELEASE);
+#undef SET_SPEEDS
+
+  set_left_run(RELEASE);
+  set_right_run(RELEASE);
   delay(1000);
 }
