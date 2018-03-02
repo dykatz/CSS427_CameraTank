a676ec8dae3cc2b359d2d399c2d97ed34afa55f5
Dylan Katz <3255634+muddmaker@users.noreply.github.com>
2018-03-02 12:26:06 -0800

    OwO

--- TreadController/TreadController.ino
+++ TreadController/TreadController.ino
@@ -1,63 +1,44 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
+#include <Servo.h>
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *left_front_mtr = AFMS.getMotor(2);
-Adafruit_DCMotor *right_front_mtr = AFMS.getMotor(1);
-Adafruit_DCMotor *left_back_mtr = AFMS.getMotor(3);
-Adafruit_DCMotor *right_back_mtr = AFMS.getMotor(4);
+Adafruit_DCMotor *left_mtr = AFMS.getMotor(1), *right_mtr = AFMS.getMotor(2);
+Servo pitch, yaw;
 int spd = 0;
 
-#define set_left_speed(s) do { \
-  left_front_mtr->setSpeed(s); \
-  left_back_mtr->setSpeed(s);  \
-} while (0)
-
-#define set_right_speed(s) do { \
-  right_front_mtr->setSpeed(s); \
-  right_back_mtr->setSpeed(s);  \
-} while (0)
-
-#define set_left_run(r) do { \
-  left_front_mtr->run(r);    \
-  left_back_mtr->run(r);     \
-} while (0)
-
-#define set_right_run(r) do { \
-  right_front_mtr->run(r);    \
-  right_back_mtr->run(r);     \
-} while (0)
-
 void setup() {
-  Serial.begin(9600);
   AFMS.begin();
 
-  set_left_run(RELEASE);
-  set_right_run(RELEASE);
+  left_mtr->run(RELEASE);
+  right_mtr->run(RELEASE);
+  pitch.attach(10);
+  yaw.attach(9);
 }
 
 void loop() {
-  set_left_run(FORWARD);
-  set_right_run(FORWARD);
+  left_mtr->run(FORWARD);
+  right_mtr->run(FORWARD);
 
 #define SET_SPEEDS(x) do { \
-  set_left_speed(x);       \
-  set_right_speed(x);      \
+  left_mtr->setSpeed(x);   \
+  right_mtr->setSpeed(x);  \
+  pitch.write(x);          \
   delay(10);               \
 } while (0)
 
-  for (spd = 0; spd < 255; ++spd) SET_SPEEDS(spd);
-  for (spd = 255; spd != 0; --spd) SET_SPEEDS(spd);
+  for (spd = 0; spd < 180; ++spd) SET_SPEEDS(spd);
+  for (spd = 180; spd != 0; --spd) SET_SPEEDS(spd);
 
-  set_left_run(BACKWARD);
-  set_right_run(BACKWARD);
+  left_mtr->run(BACKWARD);
+  right_mtr->run(BACKWARD);
 
-  for (spd = 0; spd < 255; ++spd) SET_SPEEDS(spd);
-  for (spd = 255; spd != 0; --spd) SET_SPEEDS(spd);
+  for (spd = 0; spd < 180; ++spd) SET_SPEEDS(spd);
+  for (spd = 180; spd != 0; --spd) SET_SPEEDS(spd);
 
 #undef SET_SPEEDS
 
-  set_left_run(RELEASE);
-  set_right_run(RELEASE);
+  left_mtr->run(RELEASE);
+  right_mtr->run(RELEASE);
   delay(1000);
 }
