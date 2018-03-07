701319ab0942dc7f171ab9bb0363b6f3141d03ee
IcePlasma <samuem2@uw.edu>
2018-03-06 19:41:37 -0800

    Add files via upload

--- CameraController/CameraController.ino
+++ CameraController/CameraController.ino
@@ -2,21 +2,47 @@
 
 Servo pitch, yaw;
 int pos = 0;
+int pitchPos;
+int yawPos;
 
 void setup() {
+  Serial1.begin(9600);
   Serial.begin(9600);
   pitch.attach(10);
   yaw.attach(9);
+  pitchPos = 128;
+  yawPos = 128;
 }
 
-void loop() {
-  for (pos = 0; pos <= 180; ++pos) {
-    pitch.write(pos);
-    delay(15);
-  }
+void loop()
+{
+  if(Serial1.available() > 1)
+  {
+    if(true)
+    {
+       pitchPos -= (Serial1.read() - 128)/2;
+       yawPos   += (Serial1.read() - 128)/2;
+    }
+    else
+    {
+       yawPos   += (Serial1.read() - 128)/2;
+       pitchPos -= (Serial1.read() - 128)/2;
+    }
+   
 
-  for (pos = 180; pos >= 0; pos -= 1) {
-    pitch.write(pos);
-    delay(15);
+    if(pitchPos < 0)   pitchPos = 0;
+    if(pitchPos > 255) pitchPos = 255;
+    if(yawPos < 0)     yawPos = 0;
+    if(yawPos > 255)   yawPos = 255;
+    
+    pitch.write(pitchPos);
+    yaw.write(yawPos);
+
+    Serial.write("P ");
+    Serial.print(pitchPos, DEC); 
+    Serial.write(" Y ");
+    Serial.print(yawPos, DEC); 
+    Serial.write("\n");
+    delay(5);
   }
 }
